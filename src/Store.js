2ac4018695a15419b3530611bd97a36db81340cc
mayank raghuvanshi <mayank1272@hotmail.com>
2020-05-16 19:50:52 +0530

    disable redux in production mode

--- src/Store.js
+++ src/Store.js
@@ -4,13 +4,15 @@ import rootReducer from './reducers/index'
 
 const initialState = {};
 const middleware = [thunk]
+const devTool = (process.env.NODE_ENV==='production') ?
+                applyMiddleware(...middleware) :
+                compose(
+                    applyMiddleware(...middleware),
+                    window.devToolsExtension && window.devToolsExtension()
+                )
 export default createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.devToolsExtension && window.devToolsExtension()
-    )
-
+    devTool
 )
 
